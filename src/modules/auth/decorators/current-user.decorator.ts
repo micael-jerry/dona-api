674b7506b4472739792ac3c1a830ac0109e1cc73
AshCript <asjosvah@gmail.com>
2026-09-09 11:00:34 +0300

    fix(CurrentUser decorator): allow retrieve by key sent from controller as argument to the decorator

--- src/modules/auth/decorators/current-user.decorator.ts
+++ src/modules/auth/decorators/current-user.decorator.ts
@@ -1,12 +1,18 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
-import { UserPayload } from '../payload/user.payload';
 
 /**
  * Custom decorator to extract the currently authenticated user from the request object.
  * Useful for injecting the user payload directly into controller method parameters.
  */
-export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext): UserPayload => {
+export const CurrentUser = createParamDecorator((data: string | undefined, ctx: ExecutionContext) => {
 	const request = ctx.switchToHttp().getRequest<Request>();
-	return request.user!;
+	const user = request.user;
+
+	if (!user) {
+		return null;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	return data ? user[data] : user;
 });

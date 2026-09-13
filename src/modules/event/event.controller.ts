55e535adf3197ad1e96d667c73a0ba4b89e359fb
As Manjaka Josvah Rakotondrabe <as-rakot@c1r9s5.42antananarivo.mg>
2026-09-13 17:26:54 +0300

    refactor(EventController): globalize auth decorators and remove duplicate guards

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
-import { AppAuthGuard } from '../auth/guards/app-auth.guard';
 import { AuthType } from '../auth/types/auth.type';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -10,29 +9,25 @@ import { EventEntity } from './entities/event.entity';
 import { EventService } from './event.service';
 
 @ApiTags('Events')
+@ApiBearerAuth()
 @Controller('events')
+@Auth(AuthType.AUTHENTICATED)
 export class EventController {
 	constructor(private readonly eventsService: EventService) {}
 
-	@Auth(AuthType.AUTHENTICATED)
-	@UseGuards(AppAuthGuard)
-	@Post()
-	@ApiBearerAuth()
 	@ApiOperation({ summary: 'Create a new event' })
 	@ApiResponse({
 		status: HttpStatus.CREATED,
 		description: 'The event has been created successfully.',
 		type: EventEntity,
 	})
+	@Post()
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	create(@CurrentUser('id') userId: string, @Body() createEventDto: CreateEventDto) {
 		return this.eventsService.create(userId, createEventDto);
 	}
 
-	@Auth(AuthType.AUTHENTICATED)
-	@UseGuards(AppAuthGuard)
 	@Post(':id/confirm')
-	@ApiBearerAuth()
 	@ApiOperation({
 		summary: 'Confirm that an event is still present (Encore là)',
 	})
@@ -57,8 +52,6 @@ export class EventController {
 		return this.eventsService.confirmEvent(userId, id);
 	}
 
-	@Auth(AuthType.AUTHENTICATED)
-	@UseGuards(AppAuthGuard)
 	@Get('dona')
 	@ApiOperation({ summary: 'Retrieve the list of all events' })
 	@ApiResponse({
@@ -70,8 +63,6 @@ export class EventController {
 		return this.eventsService.findAllPersonalized(userId);
 	}
 
-	@Auth(AuthType.AUTHENTICATED)
-	@UseGuards(AppAuthGuard)
 	@Get('dona/:id')
 	@ApiOperation({ summary: 'Retrieve on event with the corresponding id.' })
 	@ApiResponse({
@@ -83,8 +74,6 @@ export class EventController {
 		return this.eventsService.findOnePersonalized(userId, id);
 	}
 
-	@Auth(AuthType.AUTHENTICATED)
-	@UseGuards(AppAuthGuard)
 	@Get()
 	@ApiOperation({ summary: 'Retrieve the list of all events' })
 	@ApiResponse({
@@ -113,7 +102,6 @@ export class EventController {
 	}
 
 	@Patch(':id')
-	@ApiBearerAuth()
 	@ApiOperation({ summary: 'Update an event' })
 	@ApiParam({ name: 'id', description: 'ID of the event' })
 	@ApiResponse({
@@ -121,19 +109,18 @@ export class EventController {
 		description: 'The event has been updated.',
 		type: EventEntity,
 	})
-	update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
-		return this.eventsService.update(id, updateEventDto);
+	update(@CurrentUser('id') currentUserId: string, @Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+		return this.eventsService.update(id, currentUserId, updateEventDto);
 	}
 
 	@Delete(':id')
-	@ApiBearerAuth()
 	@ApiOperation({ summary: 'Delete an event' })
 	@ApiParam({ name: 'id', description: 'ID of the event' })
 	@ApiResponse({
 		status: HttpStatus.OK,
 		description: 'The event has been deleted.',
 	})
-	remove(@Param('id') id: string) {
-		return this.eventsService.remove(id);
+	remove(@CurrentUser('id') currentUserId: string, @Param('id') id: string) {
+		return this.eventsService.remove(id, currentUserId);
 	}
 }
